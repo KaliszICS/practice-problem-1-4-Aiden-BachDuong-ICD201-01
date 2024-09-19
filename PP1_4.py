b6c6cae5104cba80c74b25b47cf27658d9a7c6f3
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-09-19 13:48:03 +0000

    Done

--- PP1_4.py
+++ PP1_4.py
@@ -1,17 +1,33 @@
+'''
+    Lesson: Input and F strings Work
+    Author: Aiden
+    Date Created: Sept 19, 2024
+    Date Last Modified: Spet 19, 2024
+'''
 
 
 def q1():
   #Write Assignment code here
+  words = input("type a word:")
+  print(words)
 
 def q2():
   #Write Assignment code here
+  name = input("first name:")
+  print("Hello " + name)
 
 def q3():
   #Write Assignment code here
+  first = input("your first name:")
+  last = input("your last name :")
+  print(last  + first)
 
 def q4():
   #Write Assignment code here
-
+  studentOne = input("a students name:")
+  studentTwo = input("another students name:")
+  students = f"Your students are {studentOne} and {studentTwo}"
+  print(students)
 #Do not edit code below this comment
 
 q1()

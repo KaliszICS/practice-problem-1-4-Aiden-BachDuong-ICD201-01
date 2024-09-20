b3bcec0efd5b0569216163ff3ef26b8dc2664a63
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-09-20 20:46:58 +0000

    hgvuiuho

--- PP1_4.py
+++ PP1_4.py
@@ -2,37 +2,37 @@
     Lesson: Input and F strings Work
     Author: Aiden
     Date Created: Sept 19, 2024
-    Date Last Modified: Spet 19, 2024
+    Date Last Modified: Spet 20, 2024
 '''
-
 
 def q1():
   #Write Assignment code here
-  words = input("Type a word:")
+  words = input("Input a word: ")
   print(words)
 
 def q2():
   #Write Assignment code here
-  name = input("First name:")
+  name = input("Input your first name: ")
   print("Hello " + name)
 
 def q3():
   #Write Assignment code here
-  first = input("Your first name:")
-  last = input("Your last name :")
-  print(last + first)
+  first = input("Input your first name: ")
+  lastName = input("Input your last name: ")
+  print(f"{lastName} {first}")
 
 def q4():
   #Write Assignment code here
-  studentOne = input("A students name:")
-  studentTwo = input("Another students name:")
+  studentOne = input("Input a student: ")
+  studentTwo = input("Input another student: ")
   students = f"Your students are {studentOne} and {studentTwo}"
   print(students)
 
 #Do not edit code below this comment
 
-
+'''
 q1()
 q2()
 q3()
 q4()
+'''

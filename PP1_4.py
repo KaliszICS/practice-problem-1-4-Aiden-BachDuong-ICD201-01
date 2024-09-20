4d8dfbffc0f7733692eff42ec8d0805dfe1f41dc
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-09-20 13:17:42 +0000

    m jhj

--- PP1_4.py
+++ PP1_4.py
@@ -8,24 +8,24 @@
 
 def q1():
   #Write Assignment code here
-  words = input("type a word:")
+  words = input("Type a word:")
   print(words)
 
 def q2():
   #Write Assignment code here
-  name = input("first name:")
+  name = input("First name:")
   print("Hello " + name)
 
 def q3():
   #Write Assignment code here
-  first = input("your first name:")
-  last = input("your last name :")
+  first = input("Your first name:")
+  last = input("Your last name :")
   print(last + first)
 
 def q4():
   #Write Assignment code here
-  studentOne = input("a students name:")
-  studentTwo = input("another students name:")
+  studentOne = input("A students name:")
+  studentTwo = input("Another students name:")
   students = f"Your students are {studentOne} and {studentTwo}"
   print(students)
 
